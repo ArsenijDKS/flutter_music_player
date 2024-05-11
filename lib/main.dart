33d63e008dff6e86b8b30a53c8f0d77292b195c7
ArsenijDKS <ars.manag@yandex.ru>
2024-05-11 16:56:08 +0300

    modified:   lib/themes/theme_provider.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_music_player/pages/home_page.dart';
 import 'package:flutter_music_player/themes/dark_mode.dart';
 import 'package:flutter_music_player/themes/light_mode.dart';
+import 'package:flutter_music_player/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,7 +24,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
-      theme: lightMode,
+      theme: Provider.of<ThemeProvider>(context).themeData,
     );
   }
 }
---
+++ lib/themes/theme_provider.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_music_player/themes/dark_mode.dart';
+import 'package:flutter_music_player/themes/light_mode.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  // Light mode at the start
+  ThemeData _themeData = lightMode;
+
+  // Get theme
+  ThemeData get themeData => _themeData;
+
+  // Bool for dark mode
+  bool get _isDarkMode => _themeData == darkMode;
+
+  // Set theme
+  set themeData(ThemeData themeData) {
+    _themeData = themeData;
+
+    // Update UI
+    notifyListeners();
+  }
+
+  // Toggle theme
+  void toggleTheme() {
+    if (_themeData == lightMode) {
+      themeData = darkMode;
+    } else {
+      themeData = lightMode;
+    }
+    notifyListeners();
+  }
+}
