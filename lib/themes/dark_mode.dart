e3494d269c91f3ca98e54c21f8258f9f3416fb62
ArsenijDKS <ars.manag@yandex.ru>
2024-05-11 16:08:19 +0300

    new themes: dark and light themes implemented

---
+++ lib/themes/dark_mode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode = ThemeData(
+  colorScheme: ColorScheme.dark(
+    background: Colors.green.shade900,
+    primary: Colors.green.shade600,
+    secondary: Colors.green.shade800,
+    inversePrimary: Colors.green.shade300,
+  ),
+);
